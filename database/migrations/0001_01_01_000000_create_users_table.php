a9a90ce42ef4258ac74ea25b58fdd8891e4a8554
Sarah Ghoula <sarah.ghoula@etu.usthb.dz>
2026-08-30 16:23:26 +0100

    migrations +models

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('author');
+            $table->string('user_avatar')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
