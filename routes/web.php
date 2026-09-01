f320581101eefa796acd9138f79525ba98a5f1a5
Sarah Ghoula <sarah.ghoula@etu.usthb.dz>
2026-09-01 21:51:16 +0100

    login et register

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dashboard\userController;
 use App\Http\Controllers\dashboard\postController;
 use App\Http\Controllers\dashboard\categoryController;
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
   Route::get('/user', [userController::class, 'index'])->name('user');
@@ -31,3 +42,4 @@ Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
   Route::get('/user/{id}', [userController::class, 'show'])->name('showuser');
 
 });
+require __DIR__.'/auth.php';
